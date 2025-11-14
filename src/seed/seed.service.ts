dd64b61f6c973d4c3e8bd611d26ef793971f717f
Darichel Perez <darichel.perez@gmail.com>
2025-11-14 14:58:19 -0300

    feat/Http request on NestJs using axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
+import { PokemonResponse } from './interfaces/pokemon-response.interface';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return `Execute seed`;
+  private readonly axios: AxiosInstance = axios;
+  async executeSeed() {
+    const { data } = await this.axios.get<PokemonResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+    data.results.forEach(({ name, url }) => {
+      const segment = url.split('/');
+      const no = +segment[segment.length - 2];
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
 }
