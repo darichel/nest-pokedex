5ed53f398d94ac4fc5abd667d1fdebbff364cc1a
Darichel Perez <darichel.perez@gmail.com>
2025-11-14 15:09:19 -0300

    feat/Insert pokemons by lot

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { PokemonResponse } from './interfaces/pokemon-response.interface';
 import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   async executeSeed() {
     const { data } = await this.axios.get<PokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segment = url.split('/');
       const no = +segment[segment.length - 2];
+      const pokemon = await this.pokemonModel.create({ name, no });
       console.log({ name, no });
     });
 
