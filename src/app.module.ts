05ac1b7fdcb2a74901d455f67f34c17bafff2595
Darichel Perez <darichel.perez@gmail.com>
2025-11-17 11:45:59 -0300

    feat/Uses Joi validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
-import { EnvConfiguration } from './config/appp.config';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi .validation-schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
       isGlobal: true,
     }),
     ServeStaticModule.forRoot({
